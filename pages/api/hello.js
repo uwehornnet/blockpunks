4c721a39b84299955c8612ec85b4e2f7c706f3b2
uhorn <uwehornnet@users.noreply.github.com>
2022-06-26 00:07:26 +0200

    redeployed contract with changes to tokenURI

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-	console.log(req);
 	const response = {
 		name: "Crypto Dev #1",
 		description: "Crypto Dev is a collection of developers in crypto",
--- pages/api/token/[id].js
+++ pages/api/token/[id].js
@@ -1,21 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-	const referer = req.headers.referer;
-	const id = referer.split("/").pop();
-	const baseURI = referer.split("/").slice(0, 3).join("/");
-	const image = baseURI + "/static/token/" + id + ".svg";
+	const host = req.headers.host;
+	const { id } = req.query;
 
-	console.log({
-		id,
-		baseURI,
-		image,
-	});
 	const response = {
-		name: "Blockpunks #" + id,
-		description:
-			"Blockpunks is a collection of enthusiasts in web3 space. Together we are building a decentralized web3 space.",
-		image,
+		name: "Crypto Dev #1",
+		description: "Crypto Dev is a collection of developers in crypto",
+		image: `${host}/static/token/${id}.svg`,
 	};
 	res.status(200).json(response);
 }
